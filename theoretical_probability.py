6c0d2088c5712e10910c5f62a77b9cca60caccdc
GalacticGlum <nerftactic09@gmail.com>
2019-10-27 09:26:15 -0400

    Added theoretical_probability.py * This script enumerates through all possible 4-tuple combinations on the board (i.e. all the possible ways we can throw 4 marbles) and categorizes them based on their sum. * The probability distribution for getting a sum of k can be well approximated by by using a generating function and then looking at the coefficient of the k-th degree term.

---
+++ theoretical_probability.py
@@ -0,0 +1,49 @@
+"""
+Calculuates the probability that a given sum occurs.
+"""
+
+from itertools import combinations
+
+def choose(n, k):
+    """
+    A fast way to calculate binomial coefficients by Andrew Dalke (contrib).
+    """
+    if 0 <= k <= n:
+        ntok = 1
+        ktok = 1
+        for t in range(1, min(k, n - k) + 1):
+            ntok *= n
+            ktok *= t
+            n -= 1
+        return ntok // ktok
+
+    return 0
+
+
+board = [
+    4, 3, 2, 4, 1, 4, 2, 3, 4,
+    3, 4, 5, 3, 6, 3, 5, 4, 3,
+    4, 3, 2, 4, 1, 4, 2, 3, 4,
+    3, 4, 5, 3, 6, 3, 5, 4, 3,
+    4, 3, 2, 4, 1, 4, 2, 3, 4,
+    3, 4, 5, 3, 6, 3, 5, 4, 3,
+    4, 3, 2, 4, 1, 4, 2, 3, 4,
+    3, 4, 5, 3, 6, 3, 5, 4, 3,
+    4, 3, 2, 4, 1, 4, 2, 3, 4
+]
+
+event_outcomes = {}
+for outcome in combinations(board, 4):
+    value_sum = sum(outcome)
+    if value_sum not in event_outcomes:
+        event_outcomes[value_sum] = 0
+    
+    event_outcomes[value_sum] += 1
+
+probability_distribution = {}
+total_combinations = choose(len(board), 4)
+for key in event_outcomes:
+    probability_distribution[key] = event_outcomes[key] / total_combinations * 100
+
+for key in sorted(probability_distribution.keys()):
+    print(f'{key}: {probability_distribution[key]}')
